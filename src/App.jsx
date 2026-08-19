1e9d03cc6cee4ff9d74f54abbed67a60f85550c3
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-08-19 19:35:17 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
 
   
      
-      <Footer />
+    
       
       {/* Font Awesome */}
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
